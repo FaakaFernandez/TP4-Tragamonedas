f7e2eff65f8e41e63acc2e6660c53afefb7b8537
FaakaFernandez <facufern@hotmail.com>
2016-06-22 13:07:35 -0300

    Add files via upload

---
+++ test/ar/edu/unlam/tp1/testTragamonedas.java
@@ -0,0 +1,52 @@
+package ar.edu.unlam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testTragamonedas {
+
+	@Test
+	public void testFuncionamientoDelTragamonedas() {
+		Tragamonedas miTragamonedas = new Tragamonedas();
+		miTragamonedas.activar();
+		Integer posicionPrimerTambor = miTragamonedas.primerTambor.getPosicion();
+		Integer posicionSegundoTambor = miTragamonedas.segundoTambor.getPosicion();
+		Integer posicionTercerTambor = miTragamonedas.tercerTambor.getPosicion();
+		assertTrue((posicionPrimerTambor >= 1 && posicionPrimerTambor <= 10) &&
+				   (posicionSegundoTambor >= 1 && posicionSegundoTambor <= 10) &&
+				   (posicionTercerTambor >= 1 && posicionTercerTambor <= 10)
+				  );
+	}
+	
+	@Test
+	public void queLosTamboresInicienEnUno(){
+		Tragamonedas miTragamonedas = new Tragamonedas();
+		Integer valorEsperado = 1;
+		assertEquals(valorEsperado, miTragamonedas.primerTambor.getPosicion());
+		assertEquals(valorEsperado, miTragamonedas.segundoTambor.getPosicion());
+		assertEquals(valorEsperado, miTragamonedas.tercerTambor.getPosicion());
+	}
+	
+	@Test
+	public void testPremio(){
+		Tragamonedas miTragamonedas = new Tragamonedas();
+		miTragamonedas.activar();
+		
+		while(miTragamonedas.entregaPremio() == false){
+			assertFalse(miTragamonedas.entregaPremio());
+			miTragamonedas.activar();
+		}
+		if(miTragamonedas.entregaPremio() == true){
+			assertTrue(miTragamonedas.entregaPremio());
+		}
+	}
+	
+	@Test
+	public void queSeGenereUnNumeroEntreUnoYDiez() {
+		RandoNumberGenerator newGenerator = new RandoNumberGenerator();
+		Integer numeroAleatorio = newGenerator.generate();
+		assertTrue(numeroAleatorio >=1 && numeroAleatorio <= 10);
+	}
+
+}
