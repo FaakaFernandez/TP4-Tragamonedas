f2e71e5c28234ce5d859df5411f8c609c3e65f95
FaakaFernandez <facufern@hotmail.com>
2016-11-14 23:00:03 -0300

    Update FixNumberGenerator.java

--- src/FixNumberGenerator.java
+++ src/FixNumberGenerator.java
@@ -3,7 +3,7 @@ package ar.edu.unlam;
 public class FixNumberGenerator implements NumberGenerator {
 
 	private Integer number;
-
+    
 	public FixNumberGenerator(Integer number){
 		this.number = number;
 	}
